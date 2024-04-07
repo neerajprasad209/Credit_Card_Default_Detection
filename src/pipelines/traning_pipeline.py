bcc1c791ea416d0f89616bc9b971ee63addfe780
neerajprasad209 <neerajprasad.209@gmail.com>
2024-04-07 15:19:32 +0530

    Model Created

--- src/pipelines/traning_pipeline.py
+++ src/pipelines/traning_pipeline.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
 
 
 if __name__ == "__main__":
@@ -14,3 +16,8 @@ if __name__ == "__main__":
     
     data_transformation = DataTransformation()
     train_arr, test_arr,_= data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    
+    model_trainer = ModelTrainer()
+    report = model_trainer.initate_model_training(train_arr, test_arr)
+    
+    
